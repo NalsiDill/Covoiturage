063e64364ff6b6504125c3f42b74a3d994b281f6
Arnaud Strill <arnaud.strill@gmail.com>
2014-11-27 12:15:42 +0100

    Proposition de trajet
    récupérer les villes de départ puis les villes d'arrivée

--- COVOITURAGE/classes/ParcoursManager.class.php
+++ COVOITURAGE/classes/ParcoursManager.class.php
@@ -32,4 +32,40 @@ class ParcoursManager{
 		return $listeParcours;
 		
 	}
+	
+	public function getAllVilles1(){
+		$listVilles1 = array();
+		
+		$sql = 'SELECT vil_num, vil_nom FROM ville 
+		JOIN parcours on parcours.vil_num1 = ville.vil_num
+		GROUP BY vil_nom';
+		$req = $this->db->prepare($sql);
+		$req->execute();
+		
+		while($ville = $req->fetch(PDO::FETCH_OBJ)){
+			$listVilles1[] = new Ville($ville);
+		}
+		$req->closeCursor();
+		
+		return $listVilles1;
+	}
+	
+	public function getAllVilles2($no_vil1){
+		$listVilles2 = array();
+		
+		$sql = 'SELECT vil_num, vil_nom FROM ville 
+		JOIN parcours on parcours.vil_num2 = ville.vil_num
+		WHERE vil_num1 = :num
+		GROUP BY vil_nom';
+		$req->bindValue(':num', $no_vil1);
+		$req = $this->db->prepare($sql);
+		$req->execute();
+		
+		while($ville = $req->fetch(PDO::FETCH_OBJ)){
+			$listVilles2[] = new Ville($ville);
+		}
+		$req->closeCursor();
+		
+		return $listVilles2;
+	}
 }
--- COVOITURAGE/include/pages/ProposerTrajet.inc.php
+++ COVOITURAGE/include/pages/ProposerTrajet.inc.php
@@ -1,1 +1,46 @@
 <h1>Proposer un trajet</h1>
+
+<?php
+	$db = new MyPdo();
+	$pManager = new ParcoursManager($db);
+	$vManager = new VilleManager($db);
+	$villesDepart = $pManager->getAllVilles1();
+?>
+
+<?php 
+	if(empty($_POST['vil_depart'])){
+?>
+<h2>Ville de départ</h2>
+	   <form name="ajoutParcours" id="ajoutParcours" action="index.php?page=9" method="POST">
+		<select name="vil_depart">
+			<option value="0">...</option>
+			<?php
+				foreach ($villesDepart as $ville){ 
+					echo "<option value=\"".$ville->getVil_num()."\">".$ville->getVil_nom()."</option>\n";
+				} ?>
+		</select>
+		<input type="submit" value="Valider" />
+	</form>
+
+
+
+<?php
+	}else{
+		
+	$villesArrive = $pManager->getAllVilles2($_POST['vil_depart']);
+?>
+<h2>Ville d'arrivée</h2>
+	   <form name="ajoutParcours" id="ajoutParcours" action="index.php?page=9" method="POST">
+		<select name="vil_arrivee" required>
+			<option value="0">...</option>
+			<?php
+				foreach ($villesDepart as $ville){ 
+					echo "<option value=\"".$ville->getVil_num()."\">".$ville->getVil_nom()."</option>\n";
+				} ?>
+		</select>
+		<input type="submit" value="Valider" />
+	</form>
+
+<?php
+	}
+?>
