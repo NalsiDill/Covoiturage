bcd8b0eaee00ab8d5b5e4e0a74ea9131a3c71fa1
Arnaud Strill <arnaud.strill@gmail.com>
2014-12-01 09:08:50 +0100

    Suppression personne
    Suppression avec liste déroulante. OUIIIIIII!

--- COVOITURAGE/include/pages/SupprimerPersonne.inc.php
+++ COVOITURAGE/include/pages/SupprimerPersonne.inc.php
@@ -1,34 +1,33 @@
 <?php
-if (!empty($_GET["noPers"]))
-{
-	$noPers=$_GET["noPers"];
-}else{
-	$noPers=0;
-}
-
-if ($noPers == 0)
-{
-	?>
-<h1>Supprimer des personnes enregistrées</h1>
-<p>Pour effectuer une suppression, rendez-vous à la page listant les personnes.</p>
-<?php 
-} else {
 	$db = new MyPdo();
 	$pManager = new PersonneManager($db);
+	if (empty($_POST["noPers"]))
+{
+		
 	$personnes = $pManager->getAllPersonnes();
-	
-	$personne = $pManager->getPersonneByID($noPers);
-
+		
 ?>
-<h1>Supprimer des personnes enregistrées</h1>
-<p>Souhaitez-vous :
-	<br><br>
+	<h1>Supprimer des personnes enregistrées</h1>
+<form name="supprPers" id="supprPers" action="index.php?page=4" method="POST">
+
+	Supprimer :
+		<select name="noPers">
+			<option value="0">...</option>
+			<?php
+				foreach ($personnes as $personne){ 
+					echo "<option value=\"".$personne->getPer_num()."\">".$personne->getPer_nom()."</option>\n";
+				} ?>
+		</select>
+	<br/>
+	<input type="submit" value="Valider" />
+</form>
+<?php
+}else{
+		$personne = $pManager->getPersonneByID($_POST["noPers"]);
+?>
 </p>
-	<form name="supprPersonne" id="supprPersonne" action="index.php?page=1" method="post">
-		<input type="radio" name="validation" value="supprimer" required>Supprimer <?php echo $personne->getPer_prenom(); ?>  <?php echo $personne->getPer_nom(); ?> des registres ? <br/>
-		<input type="radio" name="validation" value="annuler" checked required>Revenir à la page précédente<br/><br>
-			<input type="submit" value="Valider" />
-	</form>
+  Supprimer <?php echo $personne->getPer_prenom(); ?>  <?php echo $personne->getPer_nom(); ?> des registres
+</p>
 <?php
 }
 ?>
